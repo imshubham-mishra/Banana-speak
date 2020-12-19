08eff7bfe2d2dc941fc14530b553999fa91fe798
imshubham-mishra <mishra.d.shubham@gmail.com>
2020-12-20 02:15:09 +0530

    updated final

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var outputDiv = document.querySelector('#output');
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(input) {
-    return serverURL + '?' + 'texts= ' + input
+    return serverURL + '?' + 'text='+ input;
 }
 
 function errorHandler(error) {
-    console.log("error occured ", error);
-    alert("please try again after some time!!!")
+    
+    alert("please try again after some time!!!",error);
 }
 
 function clickHandler() {
